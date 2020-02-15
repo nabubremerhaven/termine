05f853d6acb21862b6817ec69e80cc4e24d336b8
nabubremerhaven <61091241+nabubremerhaven@users.noreply.github.com>
2020-02-15 18:27:09 +0100

    Create 2020-01-02_03.js

---
+++ 2020-01-02_03.js
@@ -0,0 +1,61 @@
+ev0101 = ["ev0101", "Aktiventreff Bremerhaven-Wesermünde", "Montag, 06.01.",
+         "Infos und Planung der Aktivitäten im NABU Bremerhaven-Wesermünde",
+         "", "", "18:30", "", "NABU-Aktion zum internationalen Küstenputztag (Foto: Wierhake-Kattner)", "https://i.imgur.com/x5HSk7z.jpg"]
+
+ev0102 = ["ev0102", "Stunde der Wintervögel", "Freitag, 10.01. bis Sonntag 12.01.",
+         "Die große Zählaktion am Futterhaus, im Garten und im Park. Beobachten - melden - gewinnen.", "",
+         "<a href='https://www.nabu.de/tiere-und-pflanzen/aktionen-und-projekte/stunde-der-wintervoegel/index.html'>www.stundederwintervoegel.de</a>",
+          "1 Stunde", "", "Stunde der Wintervögel (Foto: NABU/Sebastian Hennigs)", "https://image.jimcdn.com/app/cms/image/transf/dimension=605x193:format=jpg/path/seb50942c1bfad30c/image/i9c8b29edbd5ccbfd/version/1575735798/stunde-der-winterv%C3%B6gel-foto-nabu-sebastian-hennigs.jpg"]
+
+ev0103 = ["ev0103", "Stunde der Wintervögel: Wintervögel im Bürgerpark", "Sonntag, 12.01.",
+          "Rundgang durch den Bürgerpark anlässlich der Stunde der Wintervögel.", "Vogel-AG", "", "10:00",
+          "Bürgerpark, Parkeingang Bismarckstraße, Bremerhaven", "Stunde der Wintervögel (Foto: NABU/Sebastian Hennigs)", "https://image.jimcdn.com/app/cms/image/transf/dimension=605x193:format=jpg/path/seb50942c1bfad30c/image/i9c8b29edbd5ccbfd/version/1575735798/stunde-der-winterv%C3%B6gel-foto-nabu-sebastian-hennigs.jpg"]
+
+ev0104 = ["ev0104", "NABU-Kreisverbands-Treffen", "Dienstag, 14.01.",
+         "Informationsaustausch unter den Gruppen des NABU-Kreisverbands.", "", "", "19:00", "",
+         "", ""]
+
+ev0105 = ["ev0105", "Naturtreff Bremerhaven-Wesermünde", "Montag, 20.01.",
+         "Treffen der Naturinteressierten im NABU Bremerhaven-Wesermünde", "", "", "18:30", "",
+         "Rothalstaucher (Foto: Witt)", "https://i.imgur.com/KRPCTye.jpg"]
+
+ev0106 = ["ev0106", "Fledermaus-AG Bremerhaven-Wesermünde", "Mittwoch, 22.01.",
+         "Planungstreffen. Interessierte sind herzlich willkommen!", "Fledermaus-AG", "", "19:00", "", "Großes Mausohr (Foto: NABU/Otto Schäfer)",
+         "https://image.jimcdn.com/app/cms/image/transf/dimension=605x193:format=jpg/path/seb50942c1bfad30c/image/i229d0b1574a508b4/version/1575734558/gro%C3%9Fes-mausohr-foto-nabu-otto-sch%C3%A4fer.jpg"]
+
+ev0107 = ["ev0107", "NABU aktiv bei Uthlede", "Samstag, 25.01.", "Pflegearbeiten in der Sandgrube.",
+         "Heike Wierhake-Kattner", "", "09:30", "", "Pflegearbeiten in der Sandgrube (Foto: Wierhake-Kattner)", "https://i.imgur.com/waiDOK5.jpg"]
+
+ev0202 = ["ev0202", "Grünkohlwanderung", "Sonntag, 09.02.",
+         "Grünkohlwanderung der NABU-Gruppe Bremerhaven-Wesermünde bei Wehldorf/Beverstedt. Im Anschluss Essen mit Bio-Fleisch.",
+         "", "0471-200470 (Anmeldung bis 03.02.)", "10:00 (Geschäftsstelle), 13:00 (Gaststätte)",
+         "Gaststätte ''Rauchkate'', Dohrenweg 2, Wehldorf", "Grünkohlwanderung (Foto: Wierhake-Kattner)", "https://i.imgur.com/qtMG3QB.jpg"]
+
+ev0203 = ["ev0203", "NABU aktiv bei Flögeln", "Dienstag, 11.02.", "Pflegearbeiten auf dem NABU-Grundstück",
+         "Heike Wierhake-Kattner", "", "10:00", "NABU-Grundstück in Flögeln", "Pflegearbeiten in Flögeln (Foto: Wierhake-Kattner)", "https://i.imgur.com/8WUROge.jpg"]
+
+ev0204 = ["ev0204", "Naturtreff Bremerhaven-Wesermünde", "Montag, 17.02.",
+         "Treffen der Naturinteressierten im NABU Bremerhaven-Wesermünde", "", "", "18:30", "", "Rothalstaucher (Foto: Witt)",
+         "https://i.imgur.com/KRPCTye.jpg"]
+
+ev0205 = ["ev0205", "NABU aktiv in Bramstedt", "Sonntag, 23.02.", "Pflegearbeiten auf dem NABU-Grundstück in Bramstedt",
+         "Uwe Frelich", "", "10:00", "", "", ""]
+
+ev0301 = ["ev0301", "Aktiventreff Bremerhaven-Wesermünde", "Montag, 02.03.",
+         "Infos und Planung der Aktivitäten im NABU Bremerhaven-Wesermünde.", "", "", "18:30", "", "NABU-Aktion zum internationalen Küstenputztag (Foto: Wierhake-Kattner)",
+         "https://i.imgur.com/x5HSk7z.jpg"]
+
+ev0302 = ["ev0302", "Bederkesaer See", "Sonntag, 08.03.", "Spaziergang um den Bederkesaer See", "Heike Wierhake-Kattner",
+         "", "13:30 (Geschäftsstelle), 14:00 (Bederkesa)", "Parkplatz an der Burg Bederkesa, Bederkesa", "", ""]
+
+ev0303 = ["ev0303", "Jahreshauptversammlung NABU Bremerhaven-Wesermünde", "Montag, 16.03.",
+         "Jahreshauptversammlung mit anschließendem Vortrag ''Gönn dir Garten''.", "Heike Wierhake-Kattner", "", "19:00",
+         "Kogge, Goethestraße 23, Bremerhaven", "", ""]
+
+events = [ev0101, ev0102, ev0103, ev0104, ev0105, ev0106, ev0107,
+         ev0202, ev0203, ev0204, ev0205, ev0301, ev0302, ev0303]
+
+for (let i of events) {
+    for (j = 0, len = i.length; j < len; j++) {
+        if (j < len - 1) {document.getElementById(i[0]).getElementsByTagName("p")[j].innerHTML=i[j]}
+        else {document.getElementById(i[0]).getElementsByTagName("img")[0].src=i[j]}}}
