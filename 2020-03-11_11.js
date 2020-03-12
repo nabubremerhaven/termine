077adaefb7ca4d721457b545af9cbe46a80ff767
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-03-12 19:28:43 +0100

    Update and rename 2020-03-11_10.js to 2020-03-11_11.js

--- 2020-03-11_11.js
+++ 2020-03-11_11.js
@@ -64,4 +64,3 @@ events = [ev0301, ev0302, ev0303, ev0401, ev0402, ev0403, ev0404, ev0501, ev0502
 
 for (let i of events) {
     for (j = 0, len = i.length; j < len; j++) {document.getElementById(i[0]).getElementsByTagName("p")[j].innerHTML=i[j]}
-        else {document.getElementById(i[0]).getElementsByTagName("img")[0].src=i[j]}}
