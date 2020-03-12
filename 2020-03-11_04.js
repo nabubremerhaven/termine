2bf7e76db567c04eeb0b7bf7fabf145f6c72fb6a
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-03-12 19:16:42 +0100

    Update and rename 2020-03-11_03.js to 2020-03-11_04.js

--- 2020-03-11_04.js
+++ 2020-03-11_04.js
@@ -38,8 +38,8 @@ ev0503 = ["ev0503", "Aktiventreff Bremerhaven-Wesermünde", "Montag, 06.04.",
          "", "", "18:30", "", "", ""]
          
 ev0504 = ["ev0504", "Stunde der Gartenvögel", "Freitag, 08.05. bis Sonntag, 10.05.",
-         "Die große Zählaktion in Garten und Park. Beobachten - melden - gewinnen!",
-         "<a href='https://www.stundedergartenvoegel.de/'>www.stundedergartenvoegel.de</a>", "", "1 Stunde", "bundesweit", "", ""]      
+         "Die große Zählaktion in Garten und Park. Beobachten - melden - gewinnen!", "",
+         "<a href='https://www.stundedergartenvoegel.de/'>www.stundedergartenvoegel.de</a>", "1 Stunde", "bundesweit", "", ""]      
 
 ev0505 = ["ev0505", "Naturtreff Bremerhaven-Wesermünde", "Montag, 18.05.",
          "Treffen der Naturinteressierten im NABU Bremerhaven-Wesermünde",
