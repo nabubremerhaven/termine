1d92f584fcbda240149511bc1b213ce362dc4226
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-06-23 19:02:35 +0200

    Create 2020-06-23_01.js

---
+++ 2020-06-23_01.js
@@ -0,0 +1,7 @@
+ev0301 = ["ev0601", "Bederkesaer See", "Sonntag, 28.06.",
+         "Naturkundlicher Spaziergang um den Bederkesaer See.", "A. Mülter", "Parkplatz an der Burg Bederkesa", "13:30", ""]
+
+events = [ev0601]
+
+for (let i of events) {
+    for (j = 0, len = i.length; j < len; j++) {document.getElementById(i[0]).getElementsByTagName("p")[j].innerHTML=i[j]}}
